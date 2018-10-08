85a93534bca6472f99baec1b952c8f12c0a1882a
alekssro <alekss.ro@gmail.com>
2018-10-08 19:18:27 +0200

    change usage line

--- Project03/msa_approx.py
+++ Project03/msa_approx.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Usage: ./sp_exact_3.py score_matrix 5 test.fa
+# Usage: ./msa_approx.py score_matrix 5 test.fa
 
 from Alignment import Alignment
 from Alignment import GetArguments
@@ -11,9 +11,3 @@ alignm = test.multiple_align()
 
 for i in range(len(alignm)):
     print("> ", arguments.heads[test.seqOrder[i]], "\n", test.num_to_sequence(alignm[i]), "\n", sep="")
-
-# for x in range(len(test.T)):
-#     print(test.T[x])
-# print(test.T)
-# for l in test.T:
-#     print([e[0] for e in l])
--- Project03/msa_exact.py
+++ Project03/msa_exact.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-# Usage: ./sp_exact_3.py score_matrix 5 test.fa
-
+# Usage: ./msa_exact.py score_matrix 5 test.fa
 
 from Alignment import Alignment
 from Alignment import GetArguments
@@ -13,9 +12,3 @@ alignm = test.backtrack_msa_exact()
 
 for i in range(len(alignm)):
     print("> ", arguments.heads[i], "\n", test.num_to_sequence(alignm[i]), "\n", sep="")
-
-# for x in range(len(test.T)):
-#     print(test.T[x])
-# print(test.T)
-# for l in test.T:
-#     print([e[0] for e in l])
