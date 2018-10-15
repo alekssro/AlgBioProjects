f65fdff2bfddef7fadeb15348c7f49f531885e3c
alekssro <alekss.ro@gmail.com>
2018-10-15 13:50:21 +0200

    Create files for implementation

---
+++ Project04/FiloTree.py
@@ -0,0 +1,11 @@
+class FiloTree:
+    # TODO: Define methods needed for the project
+    def __init__(self, arg):
+        pass
+
+
+class GetArguments:
+
+    # TODO: Define methods needed to read arguments
+    def __init__(self, arg):
+        pass
