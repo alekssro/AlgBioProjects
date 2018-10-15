56e0bdc0358680622fe9fce6d87a2c28f6292e0b
alekssro <alekss.ro@gmail.com>
2018-10-15 15:05:43 +0200

    Add executable path

--- Project04/rfdist.py
+++ Project04/rfdist.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python3
 ####################################################################################################
-# Program used to calculate the RF distance between two unrooted evolutionary
-# trees over the same set of species.
+# Program used to calculate the RF distance between two unrooted evolutionary trees
+# over the same set of species. We use Day's algorithm to calculate the distance.
 #
 # Input: takes two evolutionary trees in Newick format (also referred to as 'New Hampshire format')
 # Output: RF distance between the two inputed trees
@@ -14,12 +15,14 @@
 # Needed libraries
 from FiloTree import GetArguments
 from FiloTree import FiloTree
+from Bio import Phylo
 import sys
 
 # Get arguments:
 arguments = GetArguments(sys.argv)
-tree1 = arguments[0]
-tree2 = arguments[1]
+
+tree1 = arguments.trees1[0]
+tree2 = arguments.trees2[0]
 
 # Get distance
 Trees = FiloTree(tree1, tree2)
